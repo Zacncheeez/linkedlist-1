d7d1f293faf8fd6555236b5e1ae80e9c048625ad
Zacncheeez <zmt.tobin@gmail.com>
2026-01-08 08:13:33 -0800

    Update Node.h

--- edit2/Node.h
+++ edit2/Node.h
@@ -1,17 +1,24 @@
 #ifndef NODE_H
 #define NODE_H
+//Include the Student class
 #include "Student.h"
 
 #include <iostream>
 
 using namespace std;
 
+//Node class represents a single node in a linked list
 class Node{
  public:
+//Constructer
   Node(Student* newStudent);
+//Destructer
   ~Node();
+//Returns pointer to the next node
   Node* getNext();
+//Returns a pointer to the student stored in this node
   Student* getStudent();
+//Set the pointer to the next pointer in the list
   void setNext(Node* newNext);
   
  private:
