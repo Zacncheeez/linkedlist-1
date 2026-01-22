931a2602a534856c4341bbdf22b2924723df4583
Zacncheeez <zmt.tobin@gmail.com>
2026-01-22 00:41:17 -0800

    Update Node.cpp

--- edit2/Node.cpp
+++ edit2/Node.cpp
@@ -1,26 +1,28 @@
+#include <iostream>
+#include <cstring>
 #include "Node.h"
+#include "Student.h"
 
-Node::Node(Student* newStudent) {
-  student = newStudent;
-  next = nullptr;
-}
+using namespace std;
 
-Node::~Node() {
-  delete student;
-  next = nullptr;
-}
-
-
-Node* Node::getNext() {
-  return next;
+Node::Node(Student* student) {
+  this->student = student;
+  next = NULL; 
 }
 
 Student* Node::getStudent() {
   return student;
 }
 
-
-void Node::setNext(Node* newnext) {
-  next = newnext;
+void Node::setNext(Node* next) {
+  this->next = next;
 }
 
+Node* Node::getNext() {
+  return next;
+}
+
+Node::~Node() {
+  delete student;
+  next = NULL;
+}
